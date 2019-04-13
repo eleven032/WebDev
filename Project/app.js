8dbb43408d7e2a5bebdcccc52a5e081070165b8c
eleven032 <eleven032@gmail.com>
2019-04-13 15:49:59 -0400

    writing message to database

--- Project/app.js
+++ Project/app.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const taskController = require("./controllers/TaskController");
+
+// db instance connection
+require("./config/db");
+
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3301;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.listen(port,(req,res)=>{
-    console.log("test")
-})
+// API ENDPOINTS
+
+app
+  .route("/tasks")
+  .get(taskController.listAllTasks)
+  .post(taskController.createNewTask);
+
+app
+  .route("/tasks/:taskid")
+  .get(taskController.readTask)
+  .put(taskController.updateTask)
+  .delete(taskController.deleteTask);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
