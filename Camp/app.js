98e70bdd455fc1f08f02d683d8bba7351e675d51
eleven032 <eleven032@gmail.com>
2019-04-18 23:27:50 -0400

    camp web start

--- Camp/app.js
+++ Camp/app.js
@@ -1,20 +1,41 @@
 var express = require("express");
 var app = express();
-
+var bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({ extended: true }))
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.render("landing")
 });
-
+let arr = [
+    { name: "whale1", image: "https://en.pimg.jp/047/206/497/1/47206497.jpg" },
+    { name: "whale2", image: "https://en.pimg.jp/047/206/497/1/47206497.jpg" },
+    { name: "whale3", image: "https://en.pimg.jp/047/206/497/1/47206497.jpg" }
+];
 
 app.get("/campgrounds", (req, res) => {
-    let arr = [
-        { name: "whale1", image: "https://en.pimg.jp/047/206/497/1/47206497.jpg" },
-        { name: "whale2", image: "https://en.pimg.jp/047/206/497/1/47206497.jpg" },
-        { name: "whale3", image: "https://en.pimg.jp/047/206/497/1/47206497.jpg" }
-    ];
-    res.render("campgrounds", {arr:arr});
+    
+    res.render("campgrounds", { arr: arr });
 })
+
+
+app.post("/campgrounds", (req, res) => {
+    // res.send("at post part");
+    var newshit = {
+        name:req.body.name,
+        image:req.body.img
+    }
+    
+    
+    arr.push(newshit);
+    res.redirect("/campgrounds");
+})
+
+
+app.get("/campgrounds/new",(req,res)=>{
+    res.render("new");
+})
+   
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("connect");
 });
