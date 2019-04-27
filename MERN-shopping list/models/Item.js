07be84e2e6f5dd918de0562240b0aea60b0c959c
eleven032 <eleven032@gmail.com>
2019-04-27 10:39:36 -0400

    done with backend

---
+++ MERN-shopping list/models/Item.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+//create schema
+const ItemSchema = new Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    data:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+module.exports = Item = mongoose.model('Item', ItemSchema);
--- MERN-shopping list/server.js
+++ MERN-shopping list/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 
+const items = require('./routes/api/items');
 
 const app = express();
 
@@ -9,19 +10,37 @@ const app = express();
 //middleware
 app.use(bodyparser.json());
 
+
+//---------------------------------------------------
 //DB config
+mongoose
 const db = require("./config/keys").mongoURI;
 
 //connect
-mongoose.connect(db)
-    .then(()=>console.log("DB connected"))
-    .catch(err=>console.log(err));
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log("DB connected"))
+    .catch(err => console.log(err));
+//---------------------------------------------------
+
+
+
+//use routes
+app.use('/api/items', items);
+
+
+
+
+
+
+
+
+
 
 
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("Server start");
 })
 
