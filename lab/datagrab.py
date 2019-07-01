f2e02b4142b3028dec3311d4dd1845dafc86a9ec
eleven032 <eleven032@gmail.com>
2019-07-01 15:30:54 -0400

    update readfile

--- lab/datagrab.py
+++ lab/datagrab.py
@@ -2,6 +2,7 @@ import pyperclip
 from pyautogui import press, typewrite, hotkey
 import webbrowser
 import time
+import os
 
 # pyperclip.copy('let collection=document.getElementsByClassName("tl_bar");let keyword = document.getElementsByClassName("query-term query-keyword")[0].innerText;let filename= keyword.substr(0,8);filename+=".csv";var arr = [...collection];var csv = "Year,Data\\n";arr.forEach(bar => {csv += bar.title.substr(0, 9);csv += ",";csv += bar.title.substr(11).replace(/[^0-9]/ig,"");csv += "\\n";});let tmp = document.createElement("a");tmp.href = "data:text/csv;charset=utf-8," + encodeURI(csv);tmp.download = filename;document.body.appendChild(tmp);tmp.click();')
 
@@ -26,9 +27,35 @@ def wait(sec):
 #browser = webbrowser.get('firefox').open_new_tab('https://www.newspapers.com/search/#query=hurricane')
 #webbrowser.get("C:/Users/Arthur/AppData/Local/Mozilla Firefox/firefox.exe %s").open("https://www.newspapers.com/search/#query=hurricane")
 keywords = ['disaster', 'hurricane']
-location_list = ['PA', 'NY', 'FL']
+
+
+
+
+
+
 timespan = 10
 for keyword in keywords:
+    orighin_url = 'https://www.newspapers.com/search/#query=' + keyword
+    webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open(orighin_url)
+    wait(4)
+    press('f12')
+    wait(1)
+    js = 'let target = document.getElementsByClassName("see-all");target[0].children[1].click();let rows = document.getElementsByTagName("table")[2].tBodies[0].children;let arr = [...rows];for(let i =0;i<arr.length;i++){arr[i].children[0].getElementsByTagName("a")[1].click();};document.getElementById("button_Update").click();let url = window.location.href;let list = url.substr(67).split(",");function down(list) {let tmp = document.createElement("a");tmp.href = "data:text/plain;charset=utf-8," + encodeURI(list);tmp.download = "location_list";tmp.click();};down(list);'
+    copy_val(js)
+    paste_val()
+    wait(0.5)
+    press('enter')
+    wait(3)
+    press('enter')
+    wait(1)
+    hotkey('ctrl', 'w')
+    wait(4)
+    
+    text_file = open("C:/Users/Arthur/Downloads/location_list.txt", "r")
+     
+    lines = text_file.readlines()
+    location_list=lines[0].split(',')
+
     for location in location_list:
         for date in range(1751, 2031, timespan):
             url = 'https://www.newspapers.com/search/#query=' + keyword + '&dr_year=' + str(date) + '-' + str(date+timespan-1) + '&p_place=' + location
@@ -38,7 +65,7 @@ for keyword in keywords:
             #hotkey('fn', 'shift', 'k')
             wait(1)
             #js = 'var csv = "Year,Location,Data\\n";let keyword = document.getElementsByClassName("query-term query-keyword")[0].innerText;let filename = keyword.substr(0, 8);filename += ".csv";function down() {let tmp = document.createElement("a");tmp.href = "data:text/csv;charset=utf-8," + encodeURI(csv);tmp.download = filename;tmp.click();};let target = document.getElementsByClassName("see-all");target[0].children[1].click();let rows = document.getElementsByTagName("table")[2].tBodies[0].children;let arr = [...rows];arr[0].children[0].getElementsByTagName("a")[1].click();document.getElementById("button_Update").click();document.getElementById("timeline_refine_input").value = 1901 + "-" + 1950;document.getElementById("tl-gobtn").click();'
-            js = 'let d1 = ' + str(date) + '; let d2 = ' + str(date+timespan-1) +'; let keyword = document.getElementsByClassName("query-term query-keyword")[0].innerText;let filename= keyword.substr(0,8) + "_' + str(date) + '_' + str(date+timespan-1) + '";filename+=".csv"; let csv = "Year,Data\\n"; if (document.getElementsByClassName("tl_bar").length == 0) { console.log("not exist data"); } else { let collection = document.getElementsByClassName("tl_bar"); let tmp = [...collection]; tmp.forEach(bar => { let pair = bar.title.split(": "); csv += pair[0]; csv += ","; csv += pair[1].replace(/[^0-9]/ig, ""); csv += "\\n"; });let ve = document.createElement("a");ve.href = "data:text/csv;charset=utf-8," + encodeURI(csv);ve.download = filename;ve.click(); };'
+            js = 'let d1 = ' + str(date) + '; let d2 = ' + str(date+timespan-1) +'; let keyword = document.getElementsByClassName("query-term query-keyword")[0].innerText;let filename= keyword.substr(0,8) + "_' + str(location)+'_' + str(date) + '_' + str(date+timespan-1) + '";filename+=".csv"; let csv = "Year,Location,Data\\n"; if (document.getElementsByClassName("tl_bar").length == 0) { console.log("not exist data"); } else { let collection = document.getElementsByClassName("tl_bar"); let tmp = [...collection]; tmp.forEach(bar => { let pair = bar.title.split(": "); csv += pair[0]; csv += ","; csv += pair[1].replace(/[^0-9]/ig, ""); csv += "\\n"; });let ve = document.createElement("a");ve.href = "data:text/csv;charset=utf-8," + encodeURI(csv);ve.download = filename;ve.click(); };'
             copy_val(js)
             paste_val()
             wait(0.5)
--- lab/readfile.py
+++ lab/readfile.py
@@ -2,18 +2,28 @@ import pandas as pd
 import os
 import csv
 
-path = 'C:/Users/Arthur/Desktop/WebDev/lab/data'
+path = 'C:/Users/Arthur/Downloads'
 files = os.listdir(path)
-rows = []
+rows = [['Year']]
 for file in files:
         df = pd.read_csv('data/'+file)
-        start = file[9:-9]
-        end = file[14:-4]
+        tmp = file.split('_')
+        location = tmp[1]
+        start = tmp[2]
+        end = tmp[3]
         for i in range(0, len(df)):
                 if df.iloc[i][0] >= int(start) and df.iloc[i][0]<= int(end):
-                        rows.append([str(df.iloc[i][0]), str(df.iloc[i][1])])
-                        
-with open('result.csv', 'w') as csvFile:
-    writer = csv.writer(csvFile)
-    writer.writerows(rows)
-csvFile.close()
+                        rows[0].append(location)
+                        rows.append([df.iloc[i][0], df.iloc[i][1]])
+final_rows = [rows[0]]
+for i in range(1, len(rows)):
+        while(not rows[i][0] == final_rows[-1][0]+1):
+                final_rows.append([final_rows[-1][0]+1, 0])                
+        final_rows.append(rows[i])
+
+        
+
+f= open('result.csv','w+')
+for row in final_rows:
+        f.write(str(row[0]) + "," + str(row[1]) + '\n')
+f.close()
