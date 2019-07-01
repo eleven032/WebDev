9cce24d1739d1018270959d27aed6fe19d06c027
eleven032 <eleven032@gmail.com>
2019-07-01 18:30:31 -0400

    first work edition test

--- lab/datagrab.py
+++ lab/datagrab.py
@@ -35,6 +35,7 @@ keywords = ['disaster', 'hurricane']
 
 timespan = 10
 for keyword in keywords:
+    # this part is for save location list
     orighin_url = 'https://www.newspapers.com/search/#query=' + keyword
     webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open(orighin_url)
     wait(4)
@@ -51,18 +52,21 @@ for keyword in keywords:
     hotkey('ctrl', 'w')
     wait(4)
     
+
+    # save file
     text_file = open("C:/Users/Arthur/Downloads/location_list.txt", "r")
      
     lines = text_file.readlines()
     location_list=lines[0].split(',')
 
     for location in location_list:
+        # go through the location list
         for date in range(1751, 2031, timespan):
             url = 'https://www.newspapers.com/search/#query=' + keyword + '&dr_year=' + str(date) + '-' + str(date+timespan-1) + '&p_place=' + location
             webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open(url)
             wait(4)
             press('f12')
-            #hotkey('fn', 'shift', 'k')
+         
             wait(1)
             #js = 'var csv = "Year,Location,Data\\n";let keyword = document.getElementsByClassName("query-term query-keyword")[0].innerText;let filename = keyword.substr(0, 8);filename += ".csv";function down() {let tmp = document.createElement("a");tmp.href = "data:text/csv;charset=utf-8," + encodeURI(csv);tmp.download = filename;tmp.click();};let target = document.getElementsByClassName("see-all");target[0].children[1].click();let rows = document.getElementsByTagName("table")[2].tBodies[0].children;let arr = [...rows];arr[0].children[0].getElementsByTagName("a")[1].click();document.getElementById("button_Update").click();document.getElementById("timeline_refine_input").value = 1901 + "-" + 1950;document.getElementById("tl-gobtn").click();'
             js = 'let d1 = ' + str(date) + '; let d2 = ' + str(date+timespan-1) +'; let keyword = document.getElementsByClassName("query-term query-keyword")[0].innerText;let filename= keyword.substr(0,8) + "_' + str(location)+'_' + str(date) + '_' + str(date+timespan-1) + '";filename+=".csv"; let csv = "Year,Location,Data\\n"; if (document.getElementsByClassName("tl_bar").length == 0) { console.log("not exist data"); } else { let collection = document.getElementsByClassName("tl_bar"); let tmp = [...collection]; tmp.forEach(bar => { let pair = bar.title.split(": "); csv += pair[0]; csv += ","; csv += pair[1].replace(/[^0-9]/ig, ""); csv += "\\n"; });let ve = document.createElement("a");ve.href = "data:text/csv;charset=utf-8," + encodeURI(csv);ve.download = filename;ve.click(); };'
--- lab/readfile.py
+++ lab/readfile.py
@@ -2,29 +2,48 @@ import pandas as pd
 import os
 import csv
 
+# path need to be the place you save the data files set
 path = 'C:/Users/Arthur/Desktop/WebDev/lab/data'
 files = os.listdir(path)
+
+# first line first elemnt is year tag
 rows = [['Year']]
 
+
+# assign 1701-2019 as row
 for x in range(1701, 2020):
     rows.append([x])
 
 
+# get into file dir
 for file in files:
+        # file path is based on your place
     df = pd.read_csv('data/'+file)
+
+
+#     split file name by _
     tmp = file.split('_')
     location = tmp[1]
     start = tmp[2]
     end = tmp[3][:-4]
+
+#     assign 0 to each year in the very begining
     if location not in rows[0]:
         rows[0].append(location)
         for x in rows[1:]:
             x.append(0)
 
+        # check which column to insert data
+        # for example
+        # if row is like [year, fl,pa,xx]
+        # then pos is 3
+        # we only insert data at 3rd position
     pos = int(len(rows[0])-1)
-# file line
+
+    # get each line data
     for i in range(0, len(df)):
         if int(df.iloc[i][0]) >= int(start) and int(df.iloc[i][0]) <= int(end):
+                # update data
             index = int(318-(2019-int(df.iloc[i][0])))+1
             rows[index][pos] = int(df.iloc[i][1])
 
@@ -40,16 +59,14 @@ for file in files:
 #                 #                 final_rows.append([final_rows[-1][0]+1, 0])
 #                 #         final_rows.append(rows[i])
 
-
+# save to result file
 f = open('result.csv', 'w+')
 for row in rows:
     res = map(lambda a: str(a), row)
     placeholder = ''
     for ele in res:
-            placeholder+=(ele+',')
-#     f.write(str(row[0]) + "," + str(row[1]) + '\n')
+        placeholder += (ele+',')
     placeholder = placeholder[0:-1]
-    placeholder+='\n'
+    placeholder += '\n'
     f.write(placeholder)
 f.close()
-# print(rows)
