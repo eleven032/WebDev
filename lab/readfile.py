dbfb68cb7ca3ec803951c834938d4edcb4c5f0b4
eleven032 <eleven032@gmail.com>
2019-07-01 15:53:14 -0400

    data import

--- lab/readfile.py
+++ lab/readfile.py
@@ -2,9 +2,12 @@ import pandas as pd
 import os
 import csv
 
-path = 'C:/Users/Arthur/Downloads'
+path = 'C:/Users/Arthur/Desktop/WebDev/lab/data'
 files = os.listdir(path)
 rows = [['Year']]
+for x in range(1701, 2020):
+        rows.append([str(x)])
+
 for file in files:
         df = pd.read_csv('data/'+file)
         tmp = file.split('_')
@@ -14,16 +17,29 @@ for file in files:
         for i in range(0, len(df)):
                 if df.iloc[i][0] >= int(start) and df.iloc[i][0]<= int(end):
                         rows[0].append(location)
-                        rows.append([df.iloc[i][0], df.iloc[i][1]])
-final_rows = [rows[0]]
-for i in range(1, len(rows)):
-        while(not rows[i][0] == final_rows[-1][0]+1):
-                final_rows.append([final_rows[-1][0]+1, 0])                
-        final_rows.append(rows[i])
+
+print(rows)
+
+
+
+
+                        
+
+
+
+
+
+
+#                         rows.append([df.iloc[i][0], df.iloc[i][1]])
+# final_rows = [rows[0]]
+# for i in range(1, len(rows)):
+#         while(not rows[i][0] == final_rows[-1][0]+1):
+#                 final_rows.append([final_rows[-1][0]+1, 0])                
+#         final_rows.append(rows[i])
 
         
 
-f= open('result.csv','w+')
-for row in final_rows:
-        f.write(str(row[0]) + "," + str(row[1]) + '\n')
-f.close()
+# f= open('result.csv','w+')
+# for row in final_rows:
+#         f.write(str(row[0]) + "," + str(row[1]) + '\n')
+# f.close()
