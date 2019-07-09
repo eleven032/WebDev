1470941fa92ca8c99ba00ccc51ab7aad3e69c837
eleven032 <eleven032@gmail.com>
2019-07-09 12:54:13 -0400

    modify format

--- lab/datagrab.py
+++ lab/datagrab.py
@@ -41,7 +41,7 @@ for keyword in keywords:
     wait(4)
     press('f12')
     wait(1)
-    js = 'let target = document.getElementsByClassName("see-all");target[0].children[1].click();let rows = document.getElementsByTagName("table")[2].tBodies[0].children;let arr = [...rows];for(let i =0;i<arr.length;i++){arr[i].children[0].getElementsByTagName("a")[1].click();};document.getElementById("button_Update").click();let url = window.location.href;let list = url.substr(67).split(",");function down(list) {let tmp = document.createElement("a");tmp.href = "data:text/plain;charset=utf-8," + encodeURI(list);tmp.download = "location_list";tmp.click();};down(list);'
+    js = 'let target = document.getElementsByClassName("see-all");target[0].children[1].click();let rows = document.getElementsByTagName("table")[2].tBodies[0].children;let arr = [...rows];for(let i =0;i<arr.length;i++){arr[i].children[0].getElementsByTagName("a")[1].click();};document.getElementById("button_Update").click();let url = window.location.href;let list = url.substr(67).split(",");function down(list) {let tmp = document.createElement("a");tmp.href = "data:text/plain;charset=utf-8," + encodeURI(list);tmp.download = "location_list_'+keyword+'";tmp.click();};down(list);'
     copy_val(js)
     paste_val()
     wait(0.5)
@@ -52,9 +52,9 @@ for keyword in keywords:
     hotkey('ctrl', 'w')
     wait(4)
     
-
+    file_path = "C:/Users/Arthur/Downloads/location_list_"+keyword+".txt";
     # save file
-    text_file = open("C:/Users/Arthur/Downloads/location_list.txt", "r")
+    text_file = open(file_path, "r")
      
     lines = text_file.readlines()
     location_list=lines[0].split(',')
@@ -68,7 +68,8 @@ for keyword in keywords:
             press('f12')
          
             wait(1)
-            #js = 'var csv = "Year,Location,Data\\n";let keyword = document.getElementsByClassName("query-term query-keyword")[0].innerText;let filename = keyword.substr(0, 8);filename += ".csv";function down() {let tmp = document.createElement("a");tmp.href = "data:text/csv;charset=utf-8," + encodeURI(csv);tmp.download = filename;tmp.click();};let target = document.getElementsByClassName("see-all");target[0].children[1].click();let rows = document.getElementsByTagName("table")[2].tBodies[0].children;let arr = [...rows];arr[0].children[0].getElementsByTagName("a")[1].click();document.getElementById("button_Update").click();document.getElementById("timeline_refine_input").value = 1901 + "-" + 1950;document.getElementById("tl-gobtn").click();'
+            #js = 'var csv 
+            # = "Year,Location,Data\\n";let keyword = document.getElementsByClassName("query-term query-keyword")[0].innerText;let filename = keyword.substr(0, 8);filename += ".csv";function down() {let tmp = document.createElement("a");tmp.href = "data:text/csv;charset=utf-8," + encodeURI(csv);tmp.download = filename;tmp.click();};let target = document.getElementsByClassName("see-all");target[0].children[1].click();let rows = document.getElementsByTagName("table")[2].tBodies[0].children;let arr = [...rows];arr[0].children[0].getElementsByTagName("a")[1].click();document.getElementById("button_Update").click();document.getElementById("timeline_refine_input").value = 1901 + "-" + 1950;document.getElementById("tl-gobtn").click();'
             js = 'let d1 = ' + str(date) + '; let d2 = ' + str(date+timespan-1) +'; let keyword = document.getElementsByClassName("query-term query-keyword")[0].innerText;let filename= keyword.substr(0,8) + "_' + str(location)+'_' + str(date) + '_' + str(date+timespan-1) + '";filename+=".csv"; let csv = "Year,Location,Data\\n"; if (document.getElementsByClassName("tl_bar").length == 0) { console.log("not exist data"); } else { let collection = document.getElementsByClassName("tl_bar"); let tmp = [...collection]; tmp.forEach(bar => { let pair = bar.title.split(": "); csv += pair[0]; csv += ","; csv += pair[1].replace(/[^0-9]/ig, ""); csv += "\\n"; });let ve = document.createElement("a");ve.href = "data:text/csv;charset=utf-8," + encodeURI(csv);ve.download = filename;ve.click(); };'
             copy_val(js)
             paste_val()
