0b74dcebd9be38b6541b41179900b0b4886e5fe6
eleven032 <eleven032@gmail.com>
2019-07-16 14:24:01 -0400

    pass connection test

---
+++ data_visualize/back_end/db/index.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    connectionLimit:10,
+    password:'1130',
+    user:'root',
+    database:'test',
+    port:'3306',
+    host:'localhost'
+})
+
+let testdb = {};
+
+testdb.all=()=>{
+    return new Promise((resolve,reject)=>{
+        pool.query('SELECT * FROM info',(err,results)=>{
+            if(err){
+                return reject(err);
+            } 
+            return resolve(results);
+        })
+    })
+};
+
+module.exports = testdb;
--- data_visualize/back_end/routes/index.js
+++ data_visualize/back_end/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
+const db = require('../db');
 const router = express.Router();
 
-router.get('/', (req,res,next)=>{
-    res.json({"test":'test'})
+router.get('/', async(req,res,next)=>{
+    try {
+        let results = await db.all();
+        res.json(results);
+    } catch (e){
+        console.log(e);
+        res.sendStatus(500);
+    }
 })
 
 
