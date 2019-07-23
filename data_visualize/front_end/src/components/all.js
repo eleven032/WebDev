5cb9cb62a643029018dbdffefeed8b67b4557f16
eleven032 <eleven032@gmail.com>
2019-07-23 13:29:44 -0400

    optimized large data display, load in 1 sec

--- data_visualize/front_end/src/components/all.js
+++ data_visualize/front_end/src/components/all.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
-import {List} from "react-virtualized";
+import { Column, Table } from 'react-virtualized';
+import 'react-virtualized/styles.css';
 
 class Data extends Component {
   constructor(props) {
@@ -16,48 +17,49 @@ class Data extends Component {
         <td>{this.props.data.year}</td>
         <td>{this.props.data.freq}</td>
       </tr>
-    )}
+    )
+  }
 }
 
 class DataList extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {datas: []};
-    }
+  constructor(props) {
+    super(props);
+    this.state = { datas: [] };
+  }
 
-    componentDidMount() {
-        axios.get('http://localhost:4000/api')
-            .then(response => {
-              console.log(response);
-              this.setState({datas: response.data});
-            })
-            .catch(function (error) {
-              console.log(error);
-            })
-    }
+  componentDidMount() {
+    axios.get('http://localhost:4000/api')
+      .then(response => {
+        console.log(response);
+        this.setState({ datas: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
 
-    componentDidUpdate() {
-        axios.get('http://localhost:4000/api')
-        .then(response => {
-          this.setState({datas: response.data});
-        })
-        .catch(function (error) {
-          console.log(error);
-        })   
-    }
+  componentDidUpdate() {
+    axios.get('http://localhost:4000/api')
+      .then(response => {
+        this.setState({ datas: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
 
-    dataList() {
-        return this.state.datas.map(function(currentData, i) {
-            return <Data data={currentData} key={i} />;
-        });
-    }
+  // dataList() {
+  //     return this.state.datas.map(function(currentData, i) {
+  //         return <Data data={currentData} key={i} />;
+  //     });
+  // }
 
-    render() {
-        return (
-            <div>
-                <h3>Keywords List</h3>
-                <table className="table table-striped" style={{ marginTop: 20 }}>
+  render() {
+    return (
+      <div>
+        <h3>Keywords List</h3>
+        {/* <table className="table table-striped" style={{ marginTop: 20 }}>
                     <thead>
                         <tr>
                             <th>Keyword</th>
@@ -69,10 +71,40 @@ class DataList extends Component {
                     <tbody>
                         { this.dataList() }
                     </tbody>
-                </table>
-            </div>
-        )
-    }
+                </table> */}
+        <Table
+          width={300}
+          height={300}
+          headerHeight={20}
+          rowHeight={30}
+          rowCount={this.state.datas.length}
+          rowGetter={({ index }) => this.state.datas[index]}
+        >
+          <Column
+            label='Keywords'
+            dataKey='keyword'
+            width={100}
+          />
+          <Column
+            width={100}
+            label='Location'
+            dataKey='location'
+          />
+          <Column
+            width={100}
+            label='Year'
+            dataKey='year'
+          />
+          <Column
+            width={100}
+            label='Freq'
+            dataKey='freq'
+          />
+
+        </Table>
+      </div>
+    )
+  }
 }
 
 export default DataList;
